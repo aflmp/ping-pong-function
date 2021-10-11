1fc2994d36149098c1b990f25c5430b3d38afb51
Alam <afaaqalam@gmail.com>
2021-10-11 08:39:01 -0700

    deploy cf using workload-identity-federation

--- cloud-function/function.go
+++ cloud-function/function.go
@@ -16,3 +16,29 @@ func Ping(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("content-type", "application/json")
 	fmt.Fprintln(w, `{"message": "pong"}`)
 }
+
+// package function
+
+// import (
+// 	"fmt"
+// 	"net/http"
+
+// 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+// )
+
+// // count is a global variable, but only shared within a function instance.
+// var count = 0
+
+// func init() {
+// 	functions.HTTP("ExecutionCount", ExecutionCount)
+// }
+
+// // ExecutionCount is an HTTP Cloud Function that counts how many times it
+// // is executed within a specific instance.
+// func ExecutionCount(w http.ResponseWriter, r *http.Request) {
+// 	count++
+
+// 	// Note: the total function invocation count across
+// 	// all instances may not be equal to this value!
+// 	fmt.Fprintf(w, "Instance execution count: %d", count)
+// }
--- cloud-function/go.mod
+++ cloud-function/go.mod
@@ -1,3 +1,3 @@
 module github.com/aflmp/ping-pong-function/cloud-function
 
-go 1.13
+go 1.21
